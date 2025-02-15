1292f879255566f079dfaf52f484a40f62bdf8e8
Ritik-kumar261 <ritikpandey51042@gmail.com>
2025-02-15 13:55:27 +0530

    sql updated

--- ritik.sql
+++ ritik.sql
@@ -3,3 +3,17 @@ MODIFY title varchar(255) null,
 MODIFY content text null,
 MODIFY author varchar(255) null,
 MODIFY publication_date date null;
+
+
+-- daly blog table query
+
+CREATE TABLE `daily_blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `publication_date` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_c
